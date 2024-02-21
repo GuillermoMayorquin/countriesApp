94d7755ca93bda9fe33c3f48cbd08373ea3ebc88
Guillermo Mayorquin <guillermomayorquin@gmail.com>
2024-02-20 17:40:14 -0700

    Fin de leccion 121 se agrega output para la busqueda en el searchbox y que emita la entrada en la consola

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
 
 @Component({
   selector: 'countries-by-capital-page',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class ByCapitalPageComponent {
+
+
+  searchByCapital(term: string){
+    console.log("Desde ByCapitalPage");
+    console.log({ term });
+  }
 
 }
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, input } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,9 +7,19 @@ import { Component, Input, input } from '@angular/core';
 })
 export class SearchBoxComponent {
 
+
   @Input()
   public placeholder: string = "";
+
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter();
+
+  emitValue(value: string): void{
+    this.onValue.emit( value );
+  }
 
 }
 
 // ? Se agrega el input para poder agregar el placeholder como atributo de la etiqueta shared-search-box
+// ? Se agrega el Output onValue para que emita los valores que se escriban en el searchbox
+// ? Tambien se crea el metodo emitValue para que se emita el string que se pase en el
